9bf2b4542594d4e26f7a1b02d082da15dc461f2c
gb <ccha97u@gmail.com>
2016-02-24 18:25:32 +0800

    create interface folder

---
+++ 4_inferface/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import requests
+import os
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "GET":
+        # get url that the user has entered
+        try:
+            url = "hahaha" + request.args['url']
+            errors.append(url)
+           
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
