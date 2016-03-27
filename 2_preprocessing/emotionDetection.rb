fc33274341422e37581590b762825a1575968968
gb <ccha97u@gmail.com>
2016-03-27 20:41:42 +0800

    Modifying the experiemnt of experts report and the data distribution report

--- 2_preprocessing/emotionDetection.rb
+++ 2_preprocessing/emotionDetection.rb
@@ -26,7 +26,7 @@ end
 
 mongo_client = MongoClient.new("localhost", 27017)
 insert_coll = mongo_client['patients']['bipolar_experts']
-coll = mongo_client['idea']['bipolar_experts_polarity']
+coll = mongo_client['idea']['bipolar_experts_polarity_clean']
 #insert_coll = mongo_client['patients']['bb_mix']
 #coll = mongo_client['idea']['bb_mix_polarity']
 
