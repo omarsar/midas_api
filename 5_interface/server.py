323838cd0f57165ae681ed2dcf9765638094c9dd
gb <ccha97u@gmail.com>
2016-04-01 15:46:13 +0800

    The ROC can PR charts of three models performance

--- 5_interface/server.py
+++ 5_interface/server.py
@@ -80,5 +80,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(debug=False, host= '0.0.0.0',port=80)
+    #app.run(debug=True)
+    app.run(debug=False, host= '0.0.0.0',port=80)
